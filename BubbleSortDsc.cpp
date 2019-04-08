fa29e0593824a85f2ee1acd8e24c207360ae1ad3
nurinayahyusuf <48977773+nurinayahyusuf@users.noreply.github.com>
2019-04-08 19:56:32 +0800

    Update BubbleSortDsc.cpp
    Memodifikasi fungsi perulangan pada fungsi void bubblesort

--- BubbleSortDsc.cpp
+++ BubbleSortDsc.cpp
@@ -9,10 +9,10 @@ int xpp = *yp;
 void bubblesort (int arr[], int n)
 {
 int i, j;
-for (i=0; i<n-1; i++)
-for (j = 0; j<n-i-1; j++)
-if (arr[j] > arr[j+1])
-swap (&arr[j], &arr[j+1]);
+for (i=n-1; i>=0; i--)
+for (j = i+1; j<n+1; j++)
+if (arr[j] > arr[j-1])
+swap (&arr[j], &arr[j-1]);
 }
 void printarray (int arr[], int size)
 {
